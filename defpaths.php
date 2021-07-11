cf29ca79cbbb5c3bd0e341ad8a48dc4a4c9855ac
awetse <86691102+awetse@users.noreply.github.com>
2021-07-11 14:19:07 +0300

    Add files via upload

---
+++ defpaths.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+defined('_TokenTypeFile')||define('_TokenTypeFile','./---/0xdata/tokentypes/tokentypes.json');
+defined('_TokensFolder')||define('_TokensFolder','./---/0xdata/tokens/');
+defined('_Certificate')||define('_Certificate','./---/0xdata/tokens/');
+defined('_ROOT')||define('_ROOT','roottrader');
+
+?>
